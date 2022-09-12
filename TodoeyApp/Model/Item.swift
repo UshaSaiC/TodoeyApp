d21213709e7933e242e64b1ec676823151c476b7
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-12 15:55:12 +0530

    Added MVC pattern and fixed a bug wrt reusable cells issue

---
+++ TodoeyApp/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  TodoeyApp
+//
+//  Created by Usha Sai Chintha on 12/09/22.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
